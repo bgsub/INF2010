698ba961b592ddd5fd4612b8faec8715a442f29b
sebastian-villanue <sebastian.vla@hotmail.com>
2021-10-26 15:58:24 -0400

    Interview Finit

--- TP3/Interview.java
+++ TP3/Interview.java
@@ -1,9 +1,12 @@
 public class Interview {
-
+    
     //TODO: completer
     public static TreeNode solution (TreeNode t1, TreeNode t2) {
-        return null;
-    }
+        if (t1 == null && t2 == null) return null;
+        else if (t1 == null) return t2;
+        else if (t2 == null) return t1;
+        TreeNode Result = new TreeNode((t1.val - t2.val <= 0) ? t1.val : t2.val);
+        Result.left = solution(t1.left, t2.left);
+        Result.right = solution(t1.right, t2.right);
+        return Result;
 }
-
-
